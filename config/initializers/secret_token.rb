cb8f5bd1ddc92349a57ce1cad4604aa1c377d84f
Ben W. Brumfield <benwbrum@gmail.com>
2013-12-11 12:26:28 -0600

    Rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReconciliationUI::Application.config.secret_key_base = '3b8083611463af068106de26587f070950a8858cf4a9f892c851062e3a93f98f4e573f4b385fc76042970318c4a6d8dcc4a39b98ca4360904499cc72838d4e7c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReconciliationUI::Application.config.session_store :cookie_store, key: '_ReconciliationUI_session'
